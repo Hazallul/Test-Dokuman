b0f608d5ac612f0101d49aa8efdb03743bd8099c
Hazallul <hazallul@gmail.com>
2025-07-30 10:45:30 +0300

    İlk commit

---
+++ src/main/java/com/example/Deneme/DenemeApplication.java
@@ -0,0 +1,33 @@
+package com.example.Deneme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/Deneme")
+public class DenemeApplication {
+
+	private String Kaydet;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenemeApplication.class, args);}
+
+
+@PostMapping("Test")
+	public String Denemeeee (@RequestBody String test) {
+		this.Kaydet=test;
+		return test;
+}
+
+
+@GetMapping("/Merhaba")
+	public String Deneme(){
+
+		return "Merhaba DünyaAA";
+	}
+
+
+
+}
